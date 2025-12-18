31a50bce717bc944bc6ecd219d63bf1f91c3dea5
UmarRana2005 <umerrana2k05@gmail.com>
2025-12-18 11:57:00 +0500

    some recent config

--- init.cpp
+++ init.cpp
@@ -4,5 +4,10 @@ using namespace std;
 
 void initCommandExe(int argc, char *argv[])
 {
-    cout << sha1("example");
+    cout << sha1("example") << endl;
+    cout << "Repository initialized." << endl;
+    cout << "Init command executed." << endl;
+    cout << sha1("umarranag") << endl;
+    cout << sha1("qasim") << endl;
+    cout << sha1("hamid") << endl;
 }
