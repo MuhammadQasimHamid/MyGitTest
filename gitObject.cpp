0b2d2b8eb649a00e76a9321f7d6bb321fbf15c44
Muhammad Qasim Hamid <mqh.qasimhamid@gmail.com>
2025-12-19 20:15:49 +0500

    Add Serialize DeserializeConstructor  to Tree and Commit Class

--- gitObject.cpp
+++ gitObject.cpp
@@ -2,7 +2,10 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <iomanip>
 #include <openssl/sha.h>
+#include "sha1.cpp"
+#include "parser.cpp"
 using namespace std;
 
 enum GitObjectType
@@ -11,6 +14,35 @@ enum GitObjectType
     Tree,
     Commit,
     Tag
+};
+GitObjectType stoGitObjectType(string str)
+{
+    if(str == "tree") return Tree;
+    if(str == "commit") return Commit;
+    if(str == "blob") return Blob;
+    if(str == "tag") return Tag;        
+}
+struct treeEntry
+{
+    string mode;
+    GitObjectType type;
+    string hash;
+    string name;
+    treeEntry(string mode, GitObjectType type, string hash, string name)
+    {
+        this->mode = mode;
+        this->type = type;
+        this->hash = hash;
+        this->name = name;
+    }
+    void fill(string &mode, GitObjectType &type, string &hash, string &name)
+    {
+        mode = this->mode;
+        type = this->type;
+        hash = this->hash;
+        name = this->name;
+    }
+    
 };
 
 class GitObject
@@ -38,6 +70,7 @@ class BlobObject : GitObject
 {
 public:
     BlobObject(string contents) : GitObject(Blob, contents) {}
+
 };
 
 class CommitObject : GitObject
@@ -57,6 +90,26 @@ public:
         this->timeStamp = timeStamp;
         this->contents = serializeContent();
     }
+    CommitObject(string contents):GitObject(Commit, contents) // desterilize
+    {
+        vector<string> lines = split(contents, '\n');
+        for (auto l : lines)
+        {
+            vector<string> parts = split(l, ' '); // divide line into parts
+            string key = parts[0]; // heading of the part
+            if (key == "tree")
+                treeHash = parts[1];
+            else if (key == "parent")
+                parentHash.push_back(parts[1]);
+            else if (key == "author")
+            {
+                author = parts[1];
+                timeStamp = parts[2];
+            }
+            else
+                message = l;
+        }
+    }
 
     string serializeContent()
     {
@@ -70,3 +123,40 @@ public:
         return res;
     }
 };
+
+class TreeObject:GitObject
+{
+    vector<treeEntry> entires;
+    TreeObject():GitObject(Tree,"")
+    {
+        
+    }
+    TreeObject(string contents):GitObject(Tree,contents)// desterilieze (contents to TreeObject)
+    {
+        vector<string> lines = split(contents,'\n');
+        for(auto l:lines)
+        {
+            vector<string> parts = split(l,'\n');
+            entires.push_back(treeEntry(parts[0],stoGitObjectType(parts[1]),parts[2],parts[3]));
+        }
+    }
+    
+
+    string serializeContent()
+    {
+        string mode,hash,name;
+        GitObjectType type;
+        string res = "";
+        for(auto e:entires)
+        {
+            e.fill(mode, type, hash, name); // fill variables from entry
+            res += mode + " " + to_string(type) + " " + hash + " " + name + "\n";
+        }
+        return res;
+    }  
+    void addEntry(treeEntry entry)
+    {
+        entires.push_back(entry);
+    }
+};
+
---
+++ parser.cpp
@@ -0,0 +1,16 @@
+#include<vector>
+#include<iostream>
+#include<sstream>
+using namespace std;
+
+vector<string> split(string str, char ch)
+{
+    istringstream ss(str);
+    vector<string> parts;
+    string token;
+    while(getline(ss,token,ch))
+    {
+        parts.push_back(token);
+    }
+    return parts;
+}
